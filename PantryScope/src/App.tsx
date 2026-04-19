c3d18d3561c758b3b99e0e8dfd409aec7acd2dc0
Victor-ferney-valencia-cuervo <pollo123bb@gmail.com>
2026-04-19 00:55:45 +0000

    mover carpetassssss y borar la carpeta extra...

--- PantryScope/src/App.tsx
+++
@@ -1,8 +0,0 @@
-function App(){
-  return(
-    <div>
-      <h1>PantryScope — Explorador de Recetas</h1>
-    </div>
-  )
-}
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <div className="cajita1">
+        <h1 className="titulo-1-inicio">PantryScope — Explorador de Recetas</h1>
+      </div>
+      <p className="hola" style={{color:"#fff"}}>esto es malo</p>
+    </div>
+  )
+}
+export default App
