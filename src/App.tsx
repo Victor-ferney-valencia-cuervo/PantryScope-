49bd44ac1bababdba82b2e8a3dc4fc4c07bd2676
Victor-ferney-valencia-cuervo <pollo123bb@gmail.com>
2026-04-22 03:37:36 +0000

    continuacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 function App() {
   return (
     <div>
-      <div className="cajita1">
-        <h1 className="titulo-1-inicio">PantryScope — Explorador de Recetas</h1>
+      <div className="hola-mundo">
+        <h1 className="text-3xl- font-bold text-black-600 text-center">PantryScope — Explorador de Recetas</h1>
       </div>
-      <p className="hola" style={{color:"#fff"}}>esto es malo</p>
     </div>
   )
 }
